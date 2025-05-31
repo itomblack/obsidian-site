c4da91ab2eee32c6241a8f40aa2db1f0aad6c54f
itomblack <itomblack88@gmail.com>
2025-05-31 14:35:23 -0700

    Updates

---
+++ precache-manifest.98f7981a2bb57d76a099f7f0c38cbaac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534e2e4f1bd20fc3490702db625a4f21",
+    "url": "/obsidian-site/index.html"
+  },
+  {
+    "revision": "25d4c8112df4e84df2b5",
+    "url": "/obsidian-site/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e035fc516985727789a4",
+    "url": "/obsidian-site/static/js/2.17d35dc2.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/obsidian-site/static/js/2.17d35dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4c007bd7eea6a11dd0",
+    "url": "/obsidian-site/static/js/3.6b7ab727.chunk.js"
+  },
+  {
+    "revision": "25d4c8112df4e84df2b5",
+    "url": "/obsidian-site/static/js/main.41955045.chunk.js"
+  },
+  {
+    "revision": "d10b5f279c1b6c957cc6",
+    "url": "/obsidian-site/static/js/runtime-main.afcadbe5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/obsidian-site/static/media/logo.06e73328.svg"
+  }
+]);
